a00a63095dfb0db7979659a0e24d6b861dbd178e
pnvictoria <pnvictoria13.work@gmail.com>
2023-01-14 18:25:44 +0200

    Fix module "SalaryCalculator"

--- Salary Calculator/src/main/java/org/example/SalaryCalculator.java
+++ Salary Calculator/src/main/java/org/example/SalaryCalculator.java
@@ -1,12 +1,19 @@
 package org.example;
 
 public class SalaryCalculator {
+    public final static double MULTIPLIER_RER_DAY_SKIPPED_LESS = 0.85;
+    public final static double MULTIPLIER_RER_DAY_SKIPPED_NORMAL = 1.00;
+    public final static int MULTIPLIER_RER_DAY_PRODUCT_BIGGER = 13;
+    public final static int MULTIPLIER_RER_DAY_PRODUCT_NORMAL = 10;
+    public final static double MIN_SALARY = 100.00;
+    public final static double MAX_SALARY = 2000.00;
+
     public double multiplierPerDaysSkipped(int daysSkipped) {
-        return daysSkipped > 5 ? 0.85 : 1.00;
+        return daysSkipped > 5 ? MULTIPLIER_RER_DAY_SKIPPED_LESS : MULTIPLIER_RER_DAY_SKIPPED_NORMAL;
     }
 
     public int multiplierPerProductsSold(int productsSold) {
-        return productsSold > 20 ? 13 : 10;
+        return productsSold > 20 ? MULTIPLIER_RER_DAY_PRODUCT_BIGGER : MULTIPLIER_RER_DAY_PRODUCT_NORMAL;
     }
 
     public double bonusForProductSold(int productsSold) {
@@ -14,7 +21,7 @@ public class SalaryCalculator {
     }
 
     public double finalSalary(int daysSkipped, int productsSold) {
-        double result = 1000.00 * multiplierPerDaysSkipped(daysSkipped) + bonusForProductSold(productsSold);
-        return Math.min(result, 2000.00);
+        double result = MIN_SALARY * multiplierPerDaysSkipped(daysSkipped) + bonusForProductSold(productsSold);
+        return Math.min(result, MAX_SALARY);
     }
 }
