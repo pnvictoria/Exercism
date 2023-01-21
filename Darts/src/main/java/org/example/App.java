bed7cc8019f496edf0830a30a6651b664f5d379e
pnvictoria <pnvictoria13.work@gmail.com>
2023-01-21 23:29:55 +0200

    Fix module "Darts" and adding App.java

---
+++ Darts/src/main/java/org/example/App.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class App {
+    public static void main(String[] args) {
+        Darts darts = new Darts();
+        System.out.println(darts.score(-9, 9));
+    }
+}
--- Darts/src/main/java/org/example/Darts.java
+++ Darts/src/main/java/org/example/Darts.java
@@ -1,17 +1,15 @@
 package org.example;
 
 public class Darts {
-    public int score(int xOfDart, int yOfDart) {
+    public int score(double xOfDart, double yOfDart) {
         double sqrt = Math.sqrt(xOfDart * xOfDart + yOfDart * yOfDart);
-        if(sqrt < 1 || sqrt == 1) {
+        if(sqrt < 1 || sqrt == 1)
             return 10;
-        }
-        if (sqrt < 5 || sqrt == 5) {
+        if (sqrt < 5 || sqrt == 5)
             return 5;
-        }
-        if (sqrt < 10 || sqrt == 10) {
+        if (sqrt < 10 || sqrt == 10)
             return 1;
-        }
         return 0;
     }
 }
+
