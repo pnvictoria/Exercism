04d8acd4aad9b092f888cf675bb6e75e80e147e2
pnvictoria <pnvictoria13.work@gmail.com>
2023-01-21 22:27:26 +0200

    New module - resolve task "Darts"

---
+++ Darts/src/main/java/org/example/Darts.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Darts {
+    public int score(int xOfDart, int yOfDart) {
+        double sqrt = Math.sqrt(xOfDart * xOfDart + yOfDart * yOfDart);
+        if(sqrt < 1 || sqrt == 1) {
+            return 10;
+        }
+        if (sqrt < 5 || sqrt == 5) {
+            return 5;
+        }
+        if (sqrt < 10 || sqrt == 10) {
+            return 1;
+        }
+        return 0;
+    }
+}
