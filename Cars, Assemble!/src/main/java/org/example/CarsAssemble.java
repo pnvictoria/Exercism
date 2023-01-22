07949320dde74991d61f95157b365d446412d61b
pnvictoria <pnvictoria13.work@gmail.com>
2023-01-23 01:00:20 +0200

    Fix module "CarsAssemble"

--- Cars, Assemble!/src/main/java/org/example/CarsAssemble.java
+++ Cars, Assemble!/src/main/java/org/example/CarsAssemble.java
@@ -2,21 +2,22 @@ package org.example;
 
 public class CarsAssemble {
     public double productionRatePerHour(int speed) {
-        return 221.0 * speed * successRate(speed);
+        return 221.0 * speed * this.successRate(speed);
     }
+
     public int workingItemsPerMinute(int speed) {
-        return ((int)productionRatePerHour(speed)) / 60;
+        return (int)this.productionRatePerHour(speed) / 60;
     }
+
     public double successRate(int speed) {
-        if(speed >= 1 && speed <= 4){
+        if (speed >= 1 && speed <= 4) {
             return 1.0;
         } else if (speed >= 5 && speed <= 8) {
             return 0.9;
-        } else if(speed == 9) {
+        } else if (speed == 9) {
             return 0.8;
-        }else if (speed == 10) {
-            return 0.77;
+        } else {
+            return speed == 10 ? 0.77 : 0.0;
         }
-        return 0;
     }
 }
